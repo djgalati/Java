189a2bc529242222d80607af077a8a99e046ea39
DOMENICO GALATI <38479396+djgalati@users.noreply.github.com>
2019-03-26 17:02:29 -0700

    Update GenericBSTClassTest.java

--- Data Structures/BinarySearchTree/GenericBSTClassTest.java
+++ Data Structures/BinarySearchTree/GenericBSTClassTest.java
@@ -1,7 +1,10 @@
 package p7_package;
 
 import static org.junit.Assert.*;
-
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class GenericBSTClassTest
