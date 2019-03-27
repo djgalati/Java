8a13f3571e45f886a3bd843979fab682409c8350
DOMENICO GALATI <38479396+djgalati@users.noreply.github.com>
2019-03-26 17:00:41 -0700

    Update SearchDataClass.java

--- Data Structures/BinarySearchTree/SearchDataClass.java
+++ Data Structures/BinarySearchTree/SearchDataClass.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package p7_package;
 
 /**
@@ -13,40 +8,89 @@ package p7_package;
  * @author djg32
  */
 public class StateDataClass implements java.lang.Comparable<StateDataClass>{
+    
     private int population;
-    
-    
     private String state;
     
+    /**
+     * Default constructor, initializes all state data to default values
+     */
     StateDataClass()
     {
         population=0;
         state=null;
     }
     
+    /**
+     * Copy constructor, sets all data to copied object
+     * @param copiedSC StateDataClass object to be copied
+     */
     StateDataClass(StateDataClass copiedSC)
     {
         population=copiedSC.population;
         state=copiedSC.state;
     }
     
+    /**
+     * Initialization constructor, initializes state data to default values
+     * @param stateName String value with state name
+     * @param inPopulation integer population for state
+     */
     StateDataClass(String stateName, int inPopulation)
     {
         population=inPopulation;
         state=stateName;
     }
     
-    
+    /**
+     * Provides required method for comparing this object to another 
+     * StateDataClass object
+     * <p>
+     * Note: uses .length to get length of state string
+     * <p>
+     * Note: uses toLowerCase to test all characters as lower case
+     * <p>
+     * Note: returns negative value (not necessarily -1) if this data is less
+     * than other data; returns positive value (not necessarily +1) if this
+     * data is greater than other data; 
+     * @param other object of StateDataClass with which to compare
+     * @return returns zero if this item and other item are alphabetically
+     * equal and the same length
+     */
     @Override
     public int compareTo(StateDataClass other)
     {
-        return 0;
+        other.state.toLowerCase();
+        this.state.toLowerCase();
+        int length1 = other.state.length();
+        int length2 = this.state.length();
+        if(length1>length2)
+        {
+            return 1;
+        }
+        else if(length1<length2)
+        {
+            return -1;
+        }
+        else return 0;
+        
     }
     
-    
+    /**
+     * Changes character to lower case only if character was originally an
+     * upper case letter
+     * @param testCharCharacter to be tested, if it is upper case it will be
+     * converted to lower case; otherwise the testChar will be returned 
+     * unchanged
+     * @return returns the lower case version of a letter if it was an upper 
+     * case letter; otherwise, the character is returned unchanged
+     */
     char toLowerCase(char testChar)
     {
-        return 'a';
+        if(testChar>97 && testChar<122) return testChar;
+        testChar-='A';
+        testChar=+'a';
+        return testChar;
     }
     
     /**
@@ -59,6 +103,6 @@ public class StateDataClass implements java.lang.Comparable<StateDataClass>{
     @Override
     public String toString()
     {
-        return "";
+        return ("State Name: " + this.state + ", Population: " + population);
     }
 }
